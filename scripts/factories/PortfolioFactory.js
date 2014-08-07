fdea9facf8dc26fdaa61302576ea5621dba8968b
Kerry Powell <kerry.powell@stgutah.com>
2014-08-06 19:33:37 -0600

    delete everything

--- scripts/factories/PortfolioFactory.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('kerrywpowell');
-
-app.factory('PortfolioFactory', function ($http) {
-    return {
-        getFileAsText: function (url, callback) {
-            $http.get(url).success(callback);
-        }
-    };
-});
